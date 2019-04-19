6bb46b43c9bec3f9ed50bb37f2bc621d5095c204
shoulderhu <shoulderhu@gmail.com>
2019-04-19 16:42:18 +0800

    Add index.html

--- app/www/routes.py
+++ app/www/routes.py
@@ -1,17 +1,23 @@
+import os
 import sys
 from . import www
-from flask import render_template
+from flask import current_app as app, render_template, url_for, json
 from app.util.api import get
 
 
 @www.route("/")
 def index():
-    #text = get("api.index")
-    #print(type(current_app.config["API_HOST"]), file=sys.stdout)
-    #return text["data"]
-    return render_template("index.html")
+    with app.open_resource("static/json/www-index-cards.json") as f:
+        cards = json.load(f)
+    return render_template("index.html",
+                           title="Big Data",
+                           cards=cards["data"])
 
 
-@www.route("/students")
-def students():
-    return render_template("students.html")
+@www.route("/student")
+def student():
+    return render_template("student.html")
+
+#text = get("api.index")
+#print(type(current_app.config["API_HOST"]), file=sys.stdout)
+#return text["data"]
